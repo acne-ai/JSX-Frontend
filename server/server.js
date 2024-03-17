37e93939ba8b452792d14eb37604204c057ff2cc
Dhruv Batra <db3sgv@virginia.edu>
2024-03-17 15:02:05 -0400

    Update server.js path issue

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use('/build', express.static(path.join(__dirname, '../build')));
 // });
 
 // serve index.html on the route '/'
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   return res.status(200).sendFile(path.join(__dirname, '../build/index.html'));
 });
 
